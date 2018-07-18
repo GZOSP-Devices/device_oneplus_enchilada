75611d28163f0778205ab85261e94677108aa382
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-07-18 11:18:24 +0200

    enchilada: add OnePlus camera
    Change-Id: I6702ac9dcc55e8c271c3d3e0bbf0780e80ef20ee
    Signed-off-by: hardwareadictos <hardwareadictos@gmail.com>

--- device.mk
+++ device.mk
@@ -35,6 +35,16 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_PACKAGES += \
     NoCutoutOverlay
 
+# Prebuilts
+PRODUCT_PACKAGES += \
+    OnePlusCamera \
+    OnePlusCameraService
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilts/addon.d/51-oneplus-camera.sh:system/addon.d/51-oneplus-camera.sh  \
+    $(LOCAL_PATH)/prebuilts/etc/permissions/privapp-permissions-oem.xml:system/etc/permissions/privapp-permissions-oem.xml \
+    $(LOCAL_PATH)/prebuilts/lib/libopcameralib-em.so:system/lib/libopcameralib-em.so
+
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
